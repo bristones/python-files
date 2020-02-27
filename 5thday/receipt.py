bca5b270b7c2c0c6f33e89654841735cacb0501d
Brigid <bristones.brigidggee@gmail.com>
2020-02-27 09:10:53 -0800

    send

---
+++ 5thday/receipt.py
@@ -0,0 +1,15 @@
+address=input("Coding Temple Inc\n 283 franklin street\n Boston,MA")
+p1_name,p1_price=("books"),int("50")
+p2_name,p2_price=("computers, 579")
+p3_name,p3_price=("monitor, 125")
+
+total=(p1_price+p2_price+p3_price)/3
+
+print("#*50")
+print("p1_name,p1_price")
+print("p2_name,p2_price")
+print("p3_name,p3_price")
+print("-*30")
+print("total is {}".fomart(total))
+print("=*50")
+print("thank you for shopping with us")
--- week2/t1di.py
+++ week2/t1di.py
@@ -1,15 +1,24 @@
-name=input("enter name of student: ")
-reg=input("enter reg of student: ")
+students=dict()
 
-subjects=dict()
+NofStudents=int(input("enter no of students"))
+for s in range(1, NofStudents+1):
+    student=dict()
+    subjects=dict()
+    studentName=input("enter name of student: ")
+    studentReg=input("enter reg of student: ")
+    studentClass=int(input("enter class of student"))
+    student['Name'] = studentName
+    student['RegNo'] = studentReg
+    student['Class'] = studentClass 
+    students[studentReg] = student
 
-NofSubjects=int(input('Enter no of subjects'))
-for n in range(1, NofSubjects+1):
-    subjectName=input("enter name of subject")
-    score=int(input("enter score of subject"))
-    subjects[subjectName]=score
+    NofSubjects=int(input('Enter no of subjects'))
+    for n in range(1, NofSubjects+1):
+        subjectName=input("enter name of subject")
+        score=int(input("enter score of subject"))
+        subjects[subjectName]=score
+        student['scores'] = subjects
 
-print(subjects)
 
 def grade(x):
     if x >= 80:
@@ -19,9 +28,30 @@ def grade(x):
     elif x >= 40 and x <60:
         return "C"
     else:
-         return "D"
+        return "D"
 
-for k,v in subjects.items():
-    print(k, grade(v))
-    sum +=v
-    print("meanGrade is", mean(sum))
+
+def meanGrade(scores,NofSubjects):
+    return scores/NofSubjects
+    
+def schoolGrade(scores, NofStudents):
+    return scores/NofStudents
+
+
+#***********************RESULT COPY*************************
+
+#print(student.items())
+
+for k,v in students.items():
+    print("Name:",v['Name'])
+    print("Reg No.:",v['RegNo'])
+    print("Class:",v['Class'])
+    for subject,scores in v['scores'].items():
+        scores += score
+        print(subject,score,"Grade:",grade(scores))
+    print('MEAN SCORE:',meanGrade(scores,len(v['Nofsubjectsscores'])))
+    print('SCHOOL GRADE:',schoolGrade(scores,len(v['Nofstudents'])))
+
+#************************* The END******************************
+
+#print("School Grade {}".format(schoolGrade))
