5fd243dac3778770838aff7198d92ef0b8872f6d
nickmwangemi <nickmwangemi@gmail.com>
2020-03-11 00:56:10 +0300

    Refactored code

--- week4/Multiple Student OOP/multiplesStudentsIntoObjects.py
+++ week4/Multiple Student OOP/multiplesStudentsIntoObjects.py
@@ -1,54 +1,52 @@
 # multiple students
-from classStudent_mod import Student 
-from subjects_mod import Subjects 
-from student_mod import Students 
+from classStudent_mod import Student
+from subjects_mod import Subject
+from student_mod import Student
 from gradingsystem import getGrade
 from gradingsystem import getMeanScore
+from school_mod import School
 
 noOfStudents = int(input('Enter the no of students'))
 
-student=[]
 # input details for many students
-for n in range(1,noOfStudents+1):
+for s in range(noOfStudents):
+    students = []
+
     studentName = input('Enter Name of Student \n')
     regNo = input('Enter Reg Number \n')
+
     studentClass = input('Enter Class \n')
 
     schoolName = input('Enter Name of Skul \n')
     schoolAddress = input('Enter Address of Skul \n')
-    school=School(schoolName,schoolAddress)
+    school = School(schoolName, schoolAddress)
 
-    nofsubjects= int(input('Enter no. of subjects \n'))
-   
-    Subjects=[]
-    for s in range(1,nofsubjects+1):
-        subjectName=input('enter name of subject \n'.strip())
-        subjectScore=int(input('enter the score \n')#{} \n'.format(subjectScore)))
-        
-        #create an instance of the subject 
-        subjecto=subjects(subjectName,subjectScore)
-        Subjects.append(subjecto)
+    noOfSubjects = int(input('Enter no. of subjects \n'))
 
-    #create an instance of a student
-    student=students(studentName,regNo,schoolName,subject)
-    #print(student.name)
-    #print(student.schoolName)
+    for s in range(1, noOfSubjects+1):
+        subjects = []
+
+        subjectName = input('enter name of subject \n')
+        # {} \n'.format(subjectScore)))
+        subjectScore = int(input('enter the score \n'))
+        # create an instance of the subject
+        subject = Subject(subjectName, subjectScore)
+        subjects.append(subject)
+
+    # create an instance of a student
+    student = Student(studentName, regNo, school, subjects)
     students.append(student)
 
-for stude in student1:
-    print(stude.name,stude.schoolName,len(stude.subjects))
+for stud in students:
+    print("\nStudent Name: {} RegNo: {}, School: {}, Subjects: {}".format(stud.studentName, stud.regNo,
+                                                                          stud.school.schoolName, len(stud.subjects)))
     for subject in subjects:
-        print(subject.name,subject.score,'Grade',getgrade(subjectScore))
+        print("\n{} {} Grade: {}".format(subject.subjectName,
+                                         subject.subjectScore, getGrade(subjectScore)))
 
 
-
-
-
-
-    
-
-#print('************* REPORT CARD ******')
-#for k,v in students.items():
+# print('************* REPORT CARD ******')
+# for k,v in students.items():
  #   print('Name:',v['name'])
 #  print('Reg No:',v['RegNo'])
  #   print('Class:',v['class'])
@@ -56,9 +54,9 @@ for stude in student1:
    # for subject,score in v['scores'].items():
     #    totalMarks += score
      #   print(subject,score,"GRADE :",getGrade(score))
-    #print('MEAN SCORE :',getMeanScore(totalMarks,len(v['scores'])))
+    # print('MEAN SCORE :',getMeanScore(totalMarks,len(v['scores'])))
 
-    #print('MEAN GRADE :',getGrade(getMeanScore(totalMarks,len(v['scores']))))
+    # print('MEAN GRADE :',getGrade(getMeanScore(totalMarks,len(v['scores']))))
 
 
-#print('*************END REPORT CARD ******')
+# print('*************END REPORT CARD ******')
--- week4/Multiple Student OOP/subjects_mod.py
+++ week4/Multiple Student OOP/subjects_mod.py
@@ -1,9 +1,8 @@
-class Subjects:
-    def __init__(self,SubjectName,subjectScore):
-        student['scores'] = subjects
-        students[regNo] = student
+class Subject:
+    def __init__(self, subjectName, subjectScore):
+        self.subjectName = subjectName
+        self.subjectScore = subjectScore
         #self.subjectName = input('Enter name of Subject \n'.strip())
         #self.subjectSCore = int(input('Enter the score for {} \n'.format(subjectName)))
-        
 
-        #for s in range(1,nofsubjects+1):
+        # for s in range(1,nofsubjects+1):
